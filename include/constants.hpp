30cc372998c994857016cf0defdaef8071de0a11
Heredia <josemanuel.heredia@uca.es>
2024-02-02 10:20:05 +0100

    quick fix

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,34 +1,49 @@
 #pragma once
 #include <string>
 
-namespace askeleton{
-	namespace routes {
-		const std::string TEMPLATES_ROUTE = "Generator/Templates/";
-		const std::string TEST_ROUTE = "Generated/UT/";
+namespace askeleton {
+namespace routes {
+const std::string TEMPLATES_ROUTE = "Generator/Templates/";
+const std::string TEST_ROUTE = "Generated/UT/";
 
-		const std::string BT_TEMPLATES_ROUTE = TEMPLATES_ROUTE + "BoostTest.tpl";
-		const std::string TEST_FILE_ENDING = "_test.cpp";
+const std::string BT_TEMPLATES_ROUTE = TEMPLATES_ROUTE + "BoostTest.tpl";
+const std::string TEST_FILE_ENDING = "_test.cpp";
 
-		const auto generateTestClassRoute = [](const std::string &filename, const std::string &classname) {
-			return TEST_ROUTE + filename + "/" + classname + TEST_FILE_ENDING;
-		};
-		const auto generateTestFileRoute = [](const std::string &filename) {
-			return generateTestClassRoute(filename, filename);
-		};
-	} // namespace routes
+const auto generateTestClassRoute = [](const std::string &filename,
+                                       const std::string &classname) {
+    return TEST_ROUTE + filename + "/" + classname + TEST_FILE_ENDING;
+};
+const auto generateTestFileRoute = [](const std::string &filename) {
+    return generateTestClassRoute(filename, filename);
+};
+} // namespace routes
 
-	namespace fileitems {
-		const std::string CLASS_NAME = "{className}";
-		const std::string POINTER_INIT_TOKEN = "{pointerInitToken}";
-		const std::string POINTER_DESTROY_TOKEN = "{pointerDestroyToken}";
-		const std::string ASSERT = "{assert}";
-	} // namespace fileitems
-	
-	namespace errors {
-		const auto openFileError = [](const std::string &filename) {
-			return "File " + filename + " couldn't be opened. Check permissions and the route.";
-		};
-	} // namespace errors
-	
-} // namespace constant
+// TEMPLATE ITEMS
+namespace tplitems {
+const std::string CLASS_NAME = "{className}";
 
+// BOOST_TEST TEMPLATE
+const std::string POINTER_INIT_TOKEN = "{pointerInitToken}";
+const std::string POINTER_DESTROY_TOKEN = "{pointerDestroyToken}";
+const std::string ASSERT = "//{assert}";
+
+// FIXTURE TEMPLATE
+const std::string FILE_PATH = "{filePath}";
+const std::string CFG_NAME = "{cfgName}";
+const std::string DATE_OF_GENERATION = "{dateOfGeneration}";
+const std::string INCLUDES = "{includes}";
+const std::string NAMESPACES = "{namespaces}";
+const std::string READ_OBJECT = "//{readObject}";
+const std::string NEW_METHODS = "{newMethods}";
+const std::string CLASS_NAME_TEST = "{classNameTest}";
+const std::string OVERLOAD_OPERATOR = "//{overloadOperator}";
+} // namespace tplitems
+
+namespace errors {
+const auto openFileError = [](const std::string &filename) {
+    return "File " + filename +
+           " couldn't be opened. Check permissions and the route.";
+};
+} // namespace errors
+
+} // namespace askeleton
